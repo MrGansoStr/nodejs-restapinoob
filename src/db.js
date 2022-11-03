1943612542c1728f96fe739af4d43aa8b6ae1530
MrGansoStr <ccounttnro1@gmail.com>
2022-11-03 13:33:52 -0500

    credentials DB updated

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise')
 async function connectDB() {
   const connection = await mysql.createConnection({
     host:'us-east.connect.psdb.cloud',
-    user:'hlp6j3ysz125umfhxrcn',
-    password:'pscale_pw_Vto5JspPAsypDzzpG89pzvZ8z8HQtKuMYSJIwlHeLB3',
+    user:'mnvnkiuxspiwkrdl6fs3',
+    password:'pscale_pw_pb5ca4a0QdYwREo6EJyRX6V6SFk4m4THu3DUmLPlMAA',
     database:'db_test_celenpays',
     ssl: {
       rejectUnauthorized: false
